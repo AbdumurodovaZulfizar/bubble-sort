b325e9a626034ac048dcf53ee0eaae5cefebcfa8
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-04-30 18:04:45 +0500

    Created method to sort array

---
+++ bubble-sort.rb
@@ -0,0 +1,11 @@
+def bubble_sort(array)
+    i=0
+    while i < array.length
+        if array[i]>array[i+1]
+            array[i], array[i + 1] = array[i + 1], array[i]
+        end
+    end
+    return array
+end
+
+print bubble_sort([1,4,2,3,8,5])
