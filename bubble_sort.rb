70d8ca58c1cb873b1b80f962b60ff645bba730a5
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-04-30 21:27:47 +0500

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,39 +1,35 @@
-# Bubble sort method
-
+#  Bubble sort method
 def bubble_sort(array)
-    unsorted = true
-    while unsorted
-      i = 0
-      unsorted = false
-      while i < (array.length - 1) 
-          array[i], array[i + 1] = array[i + 1], array[i], unsorted = true if array[i] > array[i + 1]
-        i += 1
-      end
+  unsorted = true
+  while unsorted
+    i = 0
+    unsorted = false
+    while i < (array.length - 1)
+      array[i], array[i + 1] = array[i + 1], array[i], unsorted = true if array[i] > array[i + 1]
+      i += 1
     end
-    return array
   end
+  array
+end
 
 #   Bubble sort method with yield
-
-  def bubble_sort_by(array)
-    unsorted = true
-    while unsorted
-      i = 0
-      unsorted = false
-      while i < (array.length - 1)      
-          array[i], array[i + 1] = array[i + 1], array[i], unsorted = true if yield(array[i], array[i + 1]).positive?
-        i += 1
-      end
+def bubble_sort_by(array)
+  unsorted = true
+  while unsorted
+    i = 0
+    unsorted = false
+    while i < (array.length - 1)
+      array[i], array[i + 1] = array[i + 1], array[i], unsorted = true if yield(array[i], array[i + 1]).positive?
+      i += 1
     end
-    return array
+  end
+  array
 end
 
 # Check example
-my_array = [4,3,78,2,0,2]
-arr =  bubble_sort_by(["hi","hello","hey"]) do |left,right|
-    left.length - right.length
- end
- 
-   print bubble_sort(my_array)
-   print arr
-
+my_array = [4, 3, 78, 2, 0, 2]
+arr = bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
+print bubble_sort(my_array)
+print arr
