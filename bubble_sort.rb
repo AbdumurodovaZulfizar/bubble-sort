2a9a039c25958d90193648781810ef9c9473a385
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-04-30 18:18:51 +0500

    Corrct linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,12 @@
 def bubble_sort(array)
   i = 0
     while i < array.length
-      while array[i] > array[i + 1]
-            array[i], array[i + 1] = array[i + 1], array[i]
-        end
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+      end
+        i+=1
     end
-    array
+    return array
 end
 
 print bubble_sort([1, 4, 2, 3, 8, 5])
